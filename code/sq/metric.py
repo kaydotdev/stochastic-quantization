b3de65ba08eee704792a6054dbc69d43b66d02c2
Anton Kozyriev <sonara4mus@gmail.com>
2024-09-17 00:34:25 +0300

    test(metric): coverage for loss function and nearest element search

--- code/sq/metric.py
+++ code/sq/metric.py
@@ -3,17 +3,93 @@ from typing import Tuple
 import numpy as np
 
 
-def calculate_loss(x: np.ndarray, y: np.ndarray) -> np.float64:
-    pairwise_distance = np.linalg.norm(x[:, np.newaxis] - y, axis=-1)
+def calculate_loss(xi: np.ndarray, y: np.ndarray) -> np.float64:
+    """Calculates stochastic Wasserstein (or Kantorovich–Rubinstein) distance between distributions ξ and y:
+
+    F(y) = Σᵢ₌₁ᴵ pᵢ min₁≤k≤K d(ξᵢ, yₖ)ʳ
+
+    Parameters
+    ----------
+    xi : np.ndarray
+        The original distribution ξ with shape (N, D, ...).
+    y : np.ndarray
+        The quantized distribution y with shape (M, D, ...).
+
+    Returns
+    -------
+    np.float64
+        The calculated stochastic Wasserstein distance between distributions ξ and y.
+
+    Raises
+    ------
+    ValueError
+        If one of the distributions ξ or y is empty.
+    ValueError
+        If there is a shape mismatch between individual elements in distribution ξ and y.
+
+    Notes
+    -----
+    The function assumes uniform weights (pᵢ = 1) for all elements in the original distribution.
+    The exponent r in the formula is implicitly set to 1 in this implementation.
+    """
+
+    if xi.size == 0 or y.size == 0:
+        raise ValueError("One of the distributions `xi` or `y` is empty.")
+
+    if xi.shape[1:] != y.shape[1:]:
+        raise ValueError(
+            "The dimensions of individual elements in distribution `xi` and `y` must match. Elements in "
+            f"`xi` have shape {xi.shape[1:]}, but y elements have shape {y.shape[1:]}."
+        )
+
+    pairwise_distance = np.linalg.norm(xi[:, np.newaxis] - y, axis=-1)
     min_distance = np.min(pairwise_distance, axis=-1)
 
     return np.sum(min_distance)
 
 
 def find_nearest_element(
-    x: np.ndarray, target: np.ndarray
-) -> Tuple[np.ndarray, np.signedinteger]:
-    distance = np.linalg.norm(target - x, axis=1)
+    y: np.ndarray, target: np.ndarray
+) -> Tuple[np.ndarray, np.uint]:
+    """Searches for the nearest element in `y` to `target` based on Euclidean distance. This function computes the
+    Euclidean distance between each element in `y` and the `target`, then returns the element from `y` that has the
+    smallest distance to `target`, along with its index. The shape of an individual element of `y` must match the
+    shape of `target`.
+
+    Parameters
+    ----------
+    y : np.ndarray
+        The input tensor containing multiple elements to search from with shape (N, D, ...).
+
+    target : np.ndarray
+        The target tensor with shape (D, ...).
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.uint]
+        A tuple containing two elements:
+
+        1. np.ndarray: The nearest element found in `y` to the `target` with shape (D, ...).
+        2. np.uint: The index of the nearest element in `y`.
+
+    Raises
+    ------
+    ValueError
+        If either the `y` input tensor or the `target` tensor is empty.
+    ValueError
+        If there is a shape mismatch between individual elements in `y` and `target`.
+    """
+
+    if y.size == 0 or target.size == 0:
+        raise ValueError("Either the `y` input tensor or the `target` tensor is empty.")
+
+    if y.shape[1:] != target.shape:
+        raise ValueError(
+            "The dimensions of individual elements in `y` and `target` must match. Elements in `y` have "
+            f"shape {y.shape[1:]}, but `target` tensor has shape {target.shape}."
+        )
+
+    distance = np.linalg.norm(target - y, axis=1)
     nearest_index = np.argmin(distance)
 
-    return x[nearest_index, :], nearest_index
+    return y[nearest_index, :], nearest_index
---
+++ code/tests/test_find_nearest_element.py
@@ -0,0 +1,100 @@
+import unittest
+import numpy as np
+
+
+from sq.quantization import find_nearest_element
+
+
+class TestFindNearestElement(unittest.TestCase):
+    def setUp(self):
+        self.random_state = np.random.RandomState(seed=42)
+
+    def test_should_raise_value_error_if_shape_mismatch(self):
+        # arrange
+        x = self.random_state.random((10, 2, 2))
+        y = self.random_state.random((1, 2))
+
+        # assert
+        with self.assertRaises(ValueError):
+            # act
+            find_nearest_element(x, y)
+
+    def test_should_raise_value_error_if_different_axis(self):
+        # arrange
+        x = self.random_state.random((1, 2, 2))
+        y = self.random_state.random((1, 2, 2, 2))
+
+        # assert
+        with self.assertRaises(ValueError):
+            # act
+            find_nearest_element(x, y)
+
+    def test_should_raise_value_error_if_one_of_distributions_is_empty(self):
+        # arrange
+        x = self.random_state.random((1, 2, 2))
+        y = np.array([])
+
+        # assert
+        with self.assertRaises(ValueError):
+            # act
+            find_nearest_element(x, y)
+
+    def test_should_return_nearest_element_with_index(self):
+        # arrange
+        expected_element = np.array([0.15601864, 0.15599452])
+        expected_index = 6
+
+        x = np.array(
+            [
+                [0.30461377, 0.09767211],
+                [0.68423303, 0.44015249],
+                [0.06505159, 0.94888554],
+                [0.29214465, 0.36636184],
+                [0.18182497, 0.18340451],
+                [0.83244264, 0.21233911],
+                [0.15601864, 0.15599452],
+                [0.37454012, 0.95071431],
+            ]
+        )
+        y = np.array([0.0, 0.0])
+
+        # act
+        actual_element, actual_index = find_nearest_element(x, y)
+
+        # assert
+        np.testing.assert_allclose(
+            actual_element, expected_element, rtol=1e-3, atol=1e-3
+        )
+        self.assertEqual(actual_index, expected_index)
+
+    def test_should_return_first_index_of_multiple_nearest_elements(self):
+        # arrange
+        expected_element = np.array([0.15601864, 0.15599452])
+        expected_index = 4
+
+        x = np.array(
+            [
+                [0.30461377, 0.09767211],
+                [0.68423303, 0.44015249],
+                [0.06505159, 0.94888554],
+                [0.29214465, 0.36636184],
+                [0.15601864, 0.15599452],
+                [0.15601864, 0.15599452],
+                [0.15601864, 0.15599452],
+                [0.37454012, 0.95071431],
+            ]
+        )
+        y = np.array([0.0, 0.0])
+
+        # act
+        actual_element, actual_index = find_nearest_element(x, y)
+
+        # assert
+        np.testing.assert_allclose(
+            actual_element, expected_element, rtol=1e-3, atol=1e-3
+        )
+        self.assertEqual(actual_index, expected_index)
+
+
+if __name__ == "__main__":
+    unittest.main()
